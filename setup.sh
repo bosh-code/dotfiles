6a52f897c925ac085b004e9c973f7ed50902409a
Ryan Bosher <ryan.julian.bosher@icloud.com>
2026-03-27 16:03:01 +1300

    feat: update local and setup

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# setup.sh - Setup script for dotfiles.
+#
+
+set -euo pipefail
+
+# This file should be in the root of the dotfiles dir
+dotfiles="$(cd "$(dirname "$0")" && pwd)"
+echo "Dotfiles directory: $dotfiles"
+
+config="$HOME/.config"
+local="$HOME/.local"
+
+# Install Homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Homebrew packages.
+# brew bundle --file="$dotfiles/Brewfile"
+
+# Create symlinks
+link_path() {
+  local src="$1"
+  local dst="$2"
+  mkdir -p "$(dirname "$dst")"
+
+  # unlink "$dst" || true
+  echo "Linking $src to $dst"
+  ln -s "$src" "$dst"
+}
+
+# Link config
+rm -rf "$config"
+link_path "$dotfiles/.config" "$HOME"
+
+# Link local/share
+rm -rf "$local/share"
+link_path "$dotfiles/.local/share" "$local/share"
+
+mkdir -p "$HOME/Developer/work/worktrees"
+mkdir -p "$HOME/Developer/personal/worktrees"
+mkdir -p "$HOME/Developer/other/worktrees"
+mkdir -p "$HOME/Sites"
+
+echo "Dotfiles setup complete, restart terminal"
