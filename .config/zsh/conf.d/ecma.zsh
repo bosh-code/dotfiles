707fd5b111ffa561892a936359b18af76051115f
Ryan Bosher <ryan@bosher.co.nz>
2026-04-09 11:33:33 +1200

    chore: minor changes

--- .config/zsh/conf.d/ecma.zsh
+++ .config/zsh/conf.d/ecma.zsh
@@ -2,12 +2,15 @@
 # ecma: Everything bun, deno, node, js, ts related
 #
 
-
 # lazy alias
 alias npx="pnpm dlx"
 
 # npx skills add disable telemetry
 export DISABLE_TELEMETRY=1
 
+# PNPM
 export PNPM_HOME="$HOME/.local/share/pnpm"
 export PATH="$PNPM_HOME:$PATH"
+
+# NPM
+export NPM_CONFIG_USERCONFIG="$HOME/.local/share/npm/.npmrc"
