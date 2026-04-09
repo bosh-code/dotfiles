68929ce797e61544a460dd24ac92f9d7b4a6b6f8
Ryan Bosher <ryan@bosher.co.nz>
2026-04-10 10:12:57 +1200

    chore: mise conf update

--- .config/zsh/conf.d/mise.zsh
+++ .config/zsh/conf.d/mise.zsh
@@ -4,4 +4,7 @@ alias mpu="mp use"
 alias mpug="mpu -g"
 alias mpup="mp update"
 
-eval "$(mise activate --shims zsh)"
+eval "$(mise activate zsh)"
+# fnox & pitchfork are installed via mise, so activate after mise.
+eval "$(fnox activate zsh)"
+eval "$(pitchfork activate zsh)"
