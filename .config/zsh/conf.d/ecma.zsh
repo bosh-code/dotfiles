f0baf0c46e1c67fa16e241f6c79c8a17823df416
Ryan Bosher <ryan.julian.bosher@icloud.com>
2026-04-13 14:46:18 +1200

    chore: minor updates

--- .config/zsh/conf.d/ecma.zsh
+++ .config/zsh/conf.d/ecma.zsh
@@ -10,10 +10,10 @@ alias npx="pnpm dlx"
 export DISABLE_TELEMETRY=1
 
 # PNPM
-export PNPM_HOME="$HOME/.local/share/pnpm"
+export PNPM_HOME="$XDG_DATA_HOME/pnpm" # $HOME/.local/share/pnpm
 export PATH="$PNPM_HOME:$PATH"
 
 # NPM
-export NPM_HOME="$HOME/.local/share/npm"
+export NPM_HOME="$XDG_DATA_HOME/npm" # $HOME/.local/share/npm
 export PATH="$NPM_HOME/bin:$PATH"
 export NPM_CONFIG_USERCONFIG="$NPM_HOME/.npmrc"
