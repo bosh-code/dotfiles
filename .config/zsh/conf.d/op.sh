8e583933e328f19603d40c8dc30700725bec638b
Ryan Bosher <ryan@bosher.co.nz>
2026-04-29 13:47:17 +1200

    feat(op): update 1password env

---
+++ .config/zsh/conf.d/op.sh
@@ -0,0 +1,25 @@
+#!/bin/env bash
+#
+# op - 1Password CLI integration for shell env
+#
+
+# source ~/.config/op/plugins.sh
+source "${XDG_DATA_HOME}/op/plugins.sh"
+
+export OP_ENV="${XDG_DATA_HOME}/op/.env"
+
+# 1password env run
+alias oper="op run --env-file=\"${OP_ENV}\" --"
+
+opwrap() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: opwrap <command> [args...]"
+    return 1
+  fi
+
+  local cmd="$1"
+  shift
+
+  echo "op run --env-file=\"${OP_ENV}\" -- $cmd $*"
+  op run --env-file="${OP_ENV}" -- "$cmd" "$@"
+}
