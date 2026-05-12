06ac1be41a508e22ad66a92b3baf999c2b6025f4
Ryan Bosher <ryan@bosher.co.nz>
2026-05-12 15:18:24 +1200

    chore(zsh): add k8s aliases

---
+++ .config/zsh/conf.d/k8s.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# k8s: Kubernetes
+#
+
+alias k='kubectl'
+alias kgp='kubectl get pods'
+alias kgd='kubectl get deployments'
+alias kgs='kubectl get services'
+alias kgn='kubectl get nodes'
+alias kga='kubectl get all'
+alias kdp='kubectl describe pods'
+alias kdd='kubectl describe deployments'
+alias kds='kubectl describe services'
+alias kdn='kubectl describe nodes'
+alias kl='kubectl logs'
+alias klf='kubectl logs -f'
+alias kx='kubectl exec -it'
